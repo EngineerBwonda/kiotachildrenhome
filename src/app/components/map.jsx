b62c1d9c463f48b253b1c6e21e3d1f586db4286f
Bwonda <christopherbwonda7@gmail.com>
2026-03-18 13:13:50 +0300

    updated on the map component

--- src/app/components/map.jsx
+++ src/app/components/map.jsx
@@ -5,9 +5,9 @@ export default function Map() {
       <div className="row">
         <div className="col-md-12">
           <iframe
-            src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=karen+(nairobi)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-            width="100%"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8263760916734!2d37.04724739999999!3d-1.2776553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f6fe660be3773%3A0x49f7545c3751966d!2sMountain%20movers%20and%20restoration%20Ministries%20kamulu%20kipawa%20stage!5e0!3m2!1sen!2ske!4v1773828448364!5m2!1sen!2ske"
             height="450"
+            width="100%"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
